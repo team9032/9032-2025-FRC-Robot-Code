bb0045471efa6ad0ac37666a6c6f9451e303010b
Watermilan412 <91922787+Watermilan412@users.noreply.github.com>
2024-11-20 21:31:09 -0500

    Update to 2025 Pheonix 6

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,18 +1,17 @@
 package frc.robot;
 
-import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.SteerRequestType;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest.FieldCentric;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentric;
 
 import frc.robot.subsystems.swerve.SwerveConstants;
-
-import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
 public final class Constants {
     public static class DriverConstants {
         public static final int kDriveControllerPort = 0;
 
-        public static final double kMaxSpeed = SwerveConstants.kSpeedAt12VoltsMps;
+        public static final double kMaxSpeed = SwerveConstants.kSpeedAt12Volts.magnitude();
         public static final double kRotationRate = 4 * Math.PI;
 
         public final static FieldCentric kDriveRequest = new SwerveRequest.FieldCentric()
--- src/main/java/frc/robot/subsystems/swerve/KrakenSwerve.java
+++ src/main/java/frc/robot/subsystems/swerve/KrakenSwerve.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.swerve;
 
 import static frc.robot.subsystems.swerve.SwerveConstants.*;
 
-import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
