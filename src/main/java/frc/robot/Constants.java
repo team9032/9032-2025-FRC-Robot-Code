fdb41d96c3e6dd4e8ea2b6caaa10c3228a1ccc91
Watermilan412 <91922787+Watermilan412@users.noreply.github.com>
2024-12-19 19:44:06 -0500

    Revert "Merge branch 'main' into Add-Photonvision"
    This reverts commit c9238e2d1c4c0525e3e06ddd801863838128b60e, reversing
    changes made to 5a17154dca1b50a3cbabe79d2c6378407e12ee07.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,7 +35,7 @@ public final class Constants {
         public final static FieldCentric kDriveRequest = new FieldCentric()
             .withDeadband(0.1) // TODO tune
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
-            .withSteerRequestType(SteerRequestType.MotionMagic);
+            .withSteerRequestType(SteerRequestType.MotionMagicExpo);
     }
 
     public static class PathplannerConfig {
