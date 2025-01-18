9e8dffe5b697b22220843fd804326c85c5455f65
LP-HP <87039004+LP-HP@users.noreply.github.com>
2025-01-18 15:03:02 -0500

    PID values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -64,12 +64,12 @@ public final class Constants {
     public static class ElevatorConfigs {
         private static final MotionMagicConfigs elevatorMotionMagicConfig = new MotionMagicConfigs()
                 .withMotionMagicCruiseVelocity(1)
-                .withMotionMagicAcceleration(3)
+                .withMotionMagicAcceleration(3);
 
         private static final Slot0Configs elevatorPIDConfig = new Slot0Configs()
-                .withKP(Double.POSITIVE_INFINITY)
-                .withKI(Double.NEGATIVE_INFINITY)
-                .withKD(Double.POSITIVE_INFINITY)
+                .withKP(0)
+                .withKI(0)
+                .withKD(0)
                 .withKG(0);
         // hi HARSHIL PANDENATOR
 
