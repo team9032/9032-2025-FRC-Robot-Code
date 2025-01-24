4d3470e677b722dace8cd6ab9b9db2a929825dc8
xdrc123124 <aadi.laad@gmail.com>
2025-01-23 20:16:47 -0500

    Adding Motion Magic

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,44 +1,35 @@
 package frc.robot.subsystems;
 
-
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 
 import static frc.robot.Constants.IntakeConstants.*;
 
-
-import com.ctre.phoenix6.hardware.TalonFX;
-
 public class Intake extends SubsystemBase{
-    private final TalonFX extensionMotor;
+    private final MotionMagicVoltage armControlRequest = new MotionMagicVoltage(0.0);
+
+    private final TalonFX armMotor;
     private final TalonFX wheelMotor;
 
     public Intake(){
+        //Call arm motor, use motion magic
 
-        extensionMotor = new TalonFX(kExtensionMotorId);
-        extensionMotor.getConfigurator().apply(kMotorConfig);
+        armMotor = new TalonFX(kArmMotorId);
+        armMotor.getConfigurator().apply(kMotorConfig);
 
-        wheelMotor = new TalonFX(36);
-        wheelMotor.configFactoryDefault();
+        var talonFXConfigs = new TalonFXConfiguration();        
 
+        wheelMotor = new TalonFX(kWheelMotorId);
+        wheelMotor.getConfigurator().apply(kMotorConfig);
        
     }
-   
-    public Command resetCommand() {
-        return runOnce(this::reset).withName("Reset");
-    }
 
-    private Command setExtensionPower(double power) {
-        return runOnce(() -> extensionMotor.set(power));
+    public Command moveToGround() {
+        return runOnce(() -> armMotor.setControl(armControlRequest.withPosition(28828)));
     }
-
-    public Command disableRollers() {
-        return setRollerPower(0.0);
-    }
-
     
     @Override
     public void periodic() {
