5ea21f9c024ef0ae885bcd4a0e6e02d6f5f9703b
RahulBMay <rahul2bhargava2@gmail.com>
2025-01-26 13:31:47 -0500

    Comment

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,14 +11,14 @@ public class Intake extends SubsystemBase {
     private final MotionMagicVoltage armControlRequest = new MotionMagicVoltage(0.0);
 
     private final TalonFX armMotor;
-    private final TalonFX wheelMotor;
+    // private final TalonFX wheelMotor;
 
     public Intake() {
         armMotor = new TalonFX(kExtensionMotorID);
         armMotor.getConfigurator().apply(kArmMotorConfig);
 
-        wheelMotor = new TalonFX(kWheelMotorID);
-        wheelMotor.getConfigurator().apply(kWheelMotorConfig);
+        // wheelMotor = new TalonFX(kWheelMotorID);
+        // wheelMotor.getConfigurator().apply(kWheelMotorConfig);
 
     }
 
@@ -26,17 +26,17 @@ public class Intake extends SubsystemBase {
         return runOnce(() -> armMotor.setControl(armControlRequest.withPosition(kStowPosition)));
     }
 
-    public Command intakeCoral() {
-        return runOnce(() -> wheelMotor.set(kIntakePower));
-    }
+    // public Command intakeCoral() {
+    //     return runOnce(() -> wheelMotor.set(kIntakePower));
+    // }
 
     public Command moveToGround() {
         return runOnce(() -> armMotor.setControl(armControlRequest.withPosition(kGroundPosition)));
     }
 
-    public Command ejectCoral() {
-        return runOnce(() -> wheelMotor.set(kEjectPower));
-    }
+    // public Command ejectCoral() {
+    //     return runOnce(() -> wheelMotor.set(kEjectPower));
+    // }
 
     @Override
     public void periodic() {
