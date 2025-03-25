dfc4c9bc139dafa15232f08ad36357b180ae5064
griddy-monster <143361268+griddy-monster@users.noreply.github.com>
2025-03-25 18:40:03 -0400

    Revert "add 3 coral autos back"
    This reverts commit e9120c3e64228e713bae1867587a47ce849d5b33.

--- src/main/java/frc/robot/commands/AlignWithPose.java
+++ src/main/java/frc/robot/commands/AlignWithPose.java
@@ -2,8 +2,11 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.swerve.KrakenSwerve;
 
