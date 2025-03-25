4b4a43da11a13cbca594cfc85c9f17d5bcaff0d9
griddy-monster <143361268+griddy-monster@users.noreply.github.com>
2025-03-25 18:50:47 -0400

    Revert "Reapply "add 3 coral autos back""
    This reverts commit f33ac08f1cb7e5221880ef88cb9ecb5073fb7c2a.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -118,8 +118,6 @@ public class RobotContainer {
         autoChooser = new SendableChooser<>();
         autoChooser.addOption("4 Coral Left", Autos.fourCoralLeft(elevator, arm, endEffector, krakenSwerve, intake, indexer, false));
         autoChooser.addOption("4 Coral Right", Autos.fourCoralLeft(elevator, arm, endEffector, krakenSwerve, intake, indexer, true));
-        autoChooser.addOption("3 Coral Left", Autos.threeCoralLeft(elevator, arm, endEffector, krakenSwerve, intake, indexer, false));
-        autoChooser.addOption("3 Coral Right", Autos.threeCoralLeft(elevator, arm, endEffector, krakenSwerve, intake, indexer, true));
         autoChooser.addOption("1 Coral Center", Autos.oneCoralCenter(elevator, arm, endEffector, krakenSwerve, intake, indexer));
         autoChooser.setDefaultOption("Do Nothing", Commands.none());
 
