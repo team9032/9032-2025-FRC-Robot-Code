1574a3eaa9d4ac49d232807785feaaf31e82a18e
Watermilan412 <91922787+Watermilan412@users.noreply.github.com>
2025-09-28 14:30:29 -0400

    Heading correction removed

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -2,13 +2,10 @@ package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.swerve.KrakenSwerve;
 
 import static frc.robot.Constants.DriverConstants.*;
-import static frc.robot.Constants.PathFollowingConstants.kAlignmentRotkD;
-import static frc.robot.Constants.PathFollowingConstants.kAlignmentRotkP;
 
 public class TeleopSwerve extends Command {
     private final KrakenSwerve swerve;
@@ -17,8 +14,6 @@ public class TeleopSwerve extends Command {
     private final DoubleSupplier translationSup;
     private final DoubleSupplier strafeSup;
 
-    private final PIDController headingController;
-
     public TeleopSwerve(KrakenSwerve swerve, DoubleSupplier rotSup, DoubleSupplier translationSup, DoubleSupplier strafeSup) {
         this.swerve = swerve;
 
@@ -26,16 +21,7 @@ public class TeleopSwerve extends Command {
         this.translationSup = translationSup;
         this.strafeSup = strafeSup;
 
-        headingController = new PIDController(kAlignmentRotkP, 0, kAlignmentRotkD);
-        headingController.enableContinuousInput(-Math.PI, Math.PI);
-
         addRequirements(swerve);
-    }
-
-    @Override
-    public void initialize() {
-        headingController.reset();
-        headingController.setSetpoint(swerve.getLocalization().getCurrentPose().getRotation().getRadians());
     }
 
     @Override
@@ -49,17 +35,6 @@ public class TeleopSwerve extends Command {
         translationVal *= kMaxSpeed;
         strafeVal *= kMaxSpeed;
         rotationVal *= kRotationRate;
-
-        double currentYaw = swerve.getLocalization().getCurrentPose().getRotation().getRadians();
-        double currentRotRate = swerve.getLocalization().getCurrentVelocity().omegaRadiansPerSecond;
-
-        /* Update setpoint if input is being applied or the chassis is still rotating to prevent jittering */
-        if (Math.abs(rotationVal) > kJoystickDeadband * kRotationRate || Math.abs(currentRotRate) > kNoHeadingCorrectionRotRate) 
-            headingController.setSetpoint(currentYaw);
-
-        /* Maintain current heading if no input is being applied */
-        // else 
-        //     rotationVal = headingController.calculate(currentYaw);
 
         swerve.setControl(
             kDriveRequest.withVelocityX(translationVal)
